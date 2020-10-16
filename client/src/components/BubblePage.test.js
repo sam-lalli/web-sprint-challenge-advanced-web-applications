a4b3b951a039e151e92e34a32cde9b9d585fabd4
Sam Lalli <2sam.lalli@gmail.com>
2020-10-16 11:47:46 -0600

    test works

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -37,26 +37,3 @@ test("Fetches data and renders the bubbles", async () => {
     expect(await screen.findByText(/bubbles/i)).toBeInTheDocument()
   })
 });
-
-
-// test('fetches show data and renders data', async ()=>{
-//   mockFetchShow.mockResolvedValueOnce(episodesData);
-//   render(<App />);
-//   await waitFor(async ()=>{
-//       expect(await screen.findByText(/select a season/i)).toBeInTheDocument()
-//   })
-//   // const dropdown = screen.queryAllByTestId('dropdown')
-//   const dropdown = screen.getByText(/select a season/i);
-//   userEvent.click(dropdown)
-//   await wait();
-
-//   expect(screen.getByText(/season 1/i)).toBeInTheDocument();
-
-//   const season = screen.getByText(/season 1/i)
-//   userEvent.click(season)
-
-//   expect(screen.getByText(/Chapter One: The Vanishing of Will Byers/i)).toBeInTheDocument();
-
-  
-//   });
-// })
