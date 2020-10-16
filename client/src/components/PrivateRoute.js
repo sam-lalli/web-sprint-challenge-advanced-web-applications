38d216fe91e6c7ad8cefcfb45b449495babd1b23
Sam Lalli <2sam.lalli@gmail.com>
2020-10-16 09:40:20 -0600

    Login form completed

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return(
+        <Route
+            {...rest}
+            render={(props) => {
+                if (window.localStorage.getItem('token')) {
+                    return <Component {...props}/>
+                } else {
+                    return <Redirect to="/" />
+                }
+            }}     
+        />
+    )
+}
+
+export default PrivateRoute
+
+
+
+
+
+// import React from 'react';
+// import { Route, Redirect } from 'react-router-dom'
+
+// const PrivateRoute = ({ component: Component, ...rest}) => {
+//     return (
+//         <Route 
+//             {...rest}
+//             render={(props) => {
+//                 if (window.localStorage.getItem('token')) {
+//                     return <Component {...props} />
+//                 } else {
+//                     return <Redirect to='/login' />
+//                 }
+//             }}
+//         />
+//     );
+// };
+
+// export default PrivateRoute
